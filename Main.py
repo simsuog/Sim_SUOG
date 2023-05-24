a24bc8558949f949fd24c520d325e5f44ad119d6
simsuog <134556048+simsuog@users.noreply.github.com>
2023-05-24 23:35:10 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,19 +10,20 @@ def similarity_computation(src, input, measure):
 
     sc = SimilarityComputation(src, input, measure)
 
-    #sc.similarity_images()
+    sc.similarity_images()
 
-    sc.similarity_phenotypes()
+    # sc.similarity_phenotypes()
 
 
 if __name__ == "__main__":
 
-    #ontology = 'ontology/suog_ontology_v3.67i3.owl'
-    #input_file = 'csv/images.csv'
+    ontology = 'ontology/suog_ontology_v3.66i1.owl'
+    input_file = 'csv/images.csv'
+    
     # input_file = 'csv/phenotypes/suog.csv'
 
-    ontology = 'ontology/hp.owl'
-    input_file = 'csv/phenotypes/hpo.csv'
+    # ontology = 'ontology/hp.owl'
+    # input_file = 'csv/phenotypes/hpo.csv'
 
     similarity_computation(ontology, input_file, "sim_suog") # sim_ic, sim_resnik
 
